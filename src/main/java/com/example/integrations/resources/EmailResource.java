45346bfb992561c1595a79e723c12aa86ce2b86d
danielfitipaldi <dwfitipaldi@gmail.com>
2021-08-20 19:38:00 -0300

    Project Created, EmailDTO, EmailResource, EmailService

---
+++ src/main/java/com/example/integrations/resources/EmailResource.java
@@ -0,0 +1,26 @@
+package com.example.integrations.resources;
+
+
+import com.example.integrations.dto.EmailDTO;
+import com.example.integrations.services.EmailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/emails")
+public class EmailResource {
+
+    @Autowired
+    private EmailService service;
+
+    @PostMapping
+    public ResponseEntity<Void> send(@RequestBody EmailDTO dto){
+        service.sendEmail(dto);
+        return ResponseEntity.noContent().build();
+    }
+
+}
